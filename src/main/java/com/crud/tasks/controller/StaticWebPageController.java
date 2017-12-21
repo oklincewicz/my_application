64defb52c308e8263d853f622484110a1437596f
oklincewicz <olga.klincewicz@gmail.com>
2017-12-21 14:25:13 +0100

    Moduł 24.1

---
+++ src/main/java/com/crud/tasks/controller/StaticWebPageController.java
@@ -0,0 +1,20 @@
+package com.crud.tasks.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
+
+@Controller
+public class StaticWebPageController {
+
+    @RequestMapping("/")
+    public String index(Map<String, Object> model) {
+        model.put("variable", "My Thymeleaf variable");
+        model.put("first_request", "2 * 2 = ");
+        model.put("second_request", "2 * 2 + 2 = ");
+        model.put("third_request", "2 - 2 * 2 = ");
+        model.put("two", 2);
+        return "index";
+    }
+}
